1452e27c10c8043588c8424a6ba72a4d45c85fa5
Carlos André <cagd2002@gmail.com>
2023-08-30 18:52:03 -0300

    add roboto and iceland fonts to root project

--- src/modules/auth/LoginPage.tsx
+++ src/modules/auth/LoginPage.tsx
@@ -1,5 +1,16 @@
-import { Heading } from "@chakra-ui/react";
+import { Heading, Container, Text, Input, Button } from "@chakra-ui/react";
 
 export default function LoginPage() {
-  return <Heading>Login Page</Heading>;
+  return (
+    <Container maxW="630px" mt="150px" px="40px" border="1px solid black">
+      <Heading textAlign="center">CHAT UVV</Heading>
+      <Text fontFamily="Iceland" fontSize="24px">
+        Organize pesquisas e faça documentos de maneira prática e rápida
+      </Text>
+      <Input />
+      <Input />
+      <Button>LOG IN</Button>
+      <Text>Não tem conta? Registre-se aqui.</Text>
+    </Container>
+  )
 }
--- src/theme.ts
+++ src/theme.ts
@@ -1,7 +1,8 @@
 import { extendTheme } from "@chakra-ui/react";
-// import "@fontsource/roboto/300.css";
-// import "@fontsource/roboto/400.css";
-// import "@fontsource/roboto/700.css";
+import "@fontsource/roboto/300.css";
+import "@fontsource/roboto/400.css";
+import "@fontsource/roboto/700.css";
+import "@fontsource/iceland";
 
 export const theme = extendTheme({
   styles: {
